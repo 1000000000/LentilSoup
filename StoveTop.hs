501ab8c231bb99410a1ca4047384b52b9487a0bc
1000000000 <lthyde@gmail.com>
2018-05-03 01:30:58 -0400

    Created some data types for interpreter logic

---
+++ StoveTop.hs
@@ -0,0 +1,23 @@
+import Data.Set (Set)
+import Queue (Queue)
+
+import qualified Data.Set as Set
+import qualified Queue as Q
+
+data IdName = IdName {iden :: Integer, name :: String}
+instance Eq IdName where
+  a == b = id a == id b
+
+instance Ord IdName where
+  a < b = id a < id b
+
+data Term = BoundVar IdName | FreeVar IdName | Function IdName [Term]
+
+data Formula = Predicate IdName [Term]
+ | Not Formula
+ | And (Set Formula)
+ | Forall IdName Formula
+
+data TruthTree = TruthTree {atomics :: Set Formula, formulae :: Set Formula, freeVars :: Set Term, foralls :: Queue (Formula,(Set IdName))}
+
+

a2e1cdcebe666f71ae00cba6af565d814a4018be
1000000000 <lthyde@gmail.com>
2018-05-03 22:58:21 -0400

    Tweaked types, wrote invariant list for TruthTree

--- StoveTop.hs
+++ StoveTop.hs
@@ -4,27 +4,49 @@ import Queue (Queue)
 import qualified Data.Set as Set
 import qualified Queue as Q
 
-data IdName = IdName {iden :: Integer, name :: String}
+type Id = Integer
+
+data IdName = IdName {iden :: Id, name :: String}
 instance Eq IdName where
   a == b = id a == id b
 
 instance Ord IdName where
   a <= b = id a <= id b
 
-data Term = BoundVar IdName | FreeVar IdName | Function IdName [Term]
+data Term = BoundVar IdName | FreeVar IdName | Function IdName [Term] deriving (Eq,Ord)
 
 data Formula = Equal Term Term
              | Predicate IdName [Term]
              | Not Formula
              | And (Set Formula)
              | Forall IdName Formula
+             deriving (Eq,Ord)
 
+-- Invariants:
+--  1. closed == True iff for some Formula `a`, both `a` and Not `a` top level
+--       formulae in the TruthTree
+--  2. Predicates and their negations only appear as top level formulae in atomics
+--  3. Negations of Equals only appear as top level formulae in atomics
+--  4. Positive Equals do not appear as top level formulae in the TruthTree
+--  5. Positive Foralls only appear as top level formulae in foralls
+--  6. The snd of each element in foralls is a subset of freeVars
+--  7. The fst of each element in foralls has been decomposed with each free
+--      variable in snd of that elelement
 data TruthTree = TruthTree {
   closed   :: Bool,
   atomics  :: Set Formula,
   formulae :: Set Formula,
-  freeVars :: Set IdName,
-  foralls  :: Queue (Formula,(Set IdName))
+  freeVars :: Set Id,
+  foralls  :: Queue (Formula,(Set Id))
+}
+
+empty :: TruthTree
+empty = TruthTree {
+  closed   = False,
+  atomics  = Set.empty,
+  formulae = Set.empty,
+  freeVars = Set.empty,
+  foralls  = Q.empty
 }
 
 treeDone :: TruthTree -> Bool
